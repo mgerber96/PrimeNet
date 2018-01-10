216cd58def598223a50fbcd082fdd4a3db582e7b
Marlon <gerberm@dhbw-loerrach.de>
2018-01-10 15:39:55 +0100

    Konzept für Layout

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("BlackForest_Gruppenprojekt");
+        primaryStage.setTitle("PrimeNet");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
     }
