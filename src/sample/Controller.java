173ba727781a43a40dd739cba5059e480469f341
Marlon <gerberm@dhbw-loerrach.de>
2018-01-13 14:51:49 +0100

    Konzept für Layout

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,10 +1,11 @@
 package sample;
 
-import javafx.fxml.FXML;
+
 
 public class Controller {
 
-@FXML
+
+
 
 
 }
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,8 +5,21 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+
+import java.awt.*;
+
 
 public class Main extends Application {
+
+    Button button;
+
+
+    public static void main(String[] args)
+    {
+        launch(args);
+    }
 
     @Override
     public void start(Stage primaryStage) throws Exception{
@@ -14,10 +27,8 @@ public class Main extends Application {
         primaryStage.setTitle("PrimeNet");
         primaryStage.setScene(new Scene(root, 800, 500));
         primaryStage.show();
-    }
 
+        Button button = new Button();
 
-    public static void main(String[] args) {
-        launch(args);
     }
 }
