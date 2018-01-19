d50fd231cab4087b44a78f36e09d1c91130e6e28
Marlon <gerberm@dhbw-loerrach.de>
2018-01-19 10:38:59 +0100

    Layout erweitert

--- src/PrimeNet/Controller.java
+++ src/PrimeNet/Controller.java
@@ -1,5 +1,6 @@
 package PrimeNet;
 
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,18 +17,22 @@ import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-
 import java.awt.*;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class Controller {
+public class Controller implements Initializable {
 
-    public ComboBox<String> Kategorien;
+
+    public ComboBox<String> Categories;
     @FXML
     private javafx.scene.control.Label LabelStatus;
     @FXML
     private javafx.scene.control.TextField TextUserName;
     @FXML
     private javafx.scene.control.TextField TextPassword;
+
+
 
 
 
@@ -56,14 +61,13 @@ public class Controller {
     }
 
 
-
-    public void DropdownMenü(ActionEvent event) {
-
-        Kategorien.getItems().addAll("1", "2 ");
-
+    //Führt Default-Werte beim Start aus (zb. füllen von Tabellen etc.)
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
 
     }
 }
+
 
 
 
--- src/PrimeNet/Main.java
+++ src/PrimeNet/Main.java
@@ -5,8 +5,10 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+
 
 
 public class Main extends Application {
@@ -27,5 +29,7 @@ public class Main extends Application {
         this.PrimeNet = PrimeNet;
 
     }
+
+
 
 }
