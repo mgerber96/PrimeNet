9999406815b49e40960ef7ab6cd7c2fad82905ae
Marlon <gerberm@dhbw-loerrach.de>
2018-02-23 18:37:27 +0100

    Benutzer erstellen (noch nicht vollständig)

--- src/PrimeNet/ControllerCreateUser.java
+++ src/PrimeNet/ControllerCreateUser.java
@@ -1,6 +1,48 @@
 package PrimeNet;
 
-public class ControllerCreateUser {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class ControllerCreateUser implements Initializable {
+
+
+    public Button CreateNewUser;
+    public TextField CreateUserUsername;
+    public PasswordField CreateUserPassword;
+
+    @FXML
+    private void CreateNewUser(ActionEvent e) throws IOException {
+
+        StringBuilder sbU = new StringBuilder();
+        StringBuilder sbP = new StringBuilder();
+
+        sbU.append(CreateUserUsername.getText().toString());
+        sbP.append(CreateUserPassword.getText().toString());
+        File Username = new File("Username.txt");
+        File Password = new File("Password.txt");
+        FileWriter U = new FileWriter(Username);
+        FileWriter P = new FileWriter(Password);
+        U.write(sbU.toString());
+        P.write(sbP.toString());
+        P.close();
+        U.close();
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+
+
+    }
 
 
 }
--- src/PrimeNet/ControllerForLogin.java
+++ src/PrimeNet/ControllerForLogin.java
@@ -14,6 +14,8 @@ import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -31,51 +33,77 @@ public class ControllerForLogin implements Initializable {
 
     //login with username and password
     public void Login(javafx.event.ActionEvent event) throws Exception {
+        try {
+            FileReader UsernameFile = new FileReader("UserName.txt");
+            FileReader PasswordFile = new FileReader("Password.txt");
+            BufferedReader UsernameReader = new BufferedReader(UsernameFile);
+            BufferedReader PasswordReader = new BufferedReader(PasswordFile);
+            String Usernameline = UsernameReader.readLine();
+            String Passwordline = PasswordReader.readLine();
+            String text1 = "";
+            String text2 = "";
 
-        if (TextUserName.getText().equals("") && TextPassword.getText().equals("")) {
+            while (Usernameline != null) {
 
-            Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/MainWindow.fxml"));
-            Stage PrimeNet = new Stage();
-            PrimeNet.setTitle("PrimeNet");
-            Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
-            //PrimeNet.setResizable(false);
-            //PrimeNet.setScene(new Scene(root, screen.width, screen.height));
-            PrimeNet.setScene(new Scene(root, screen.width / 1.5, screen.height / 1.1 - 65));
-            //PrimeNet.setScene(new Scene(root,800,600));
-            PrimeNet.show();
-            Main.Login.close();
-        } else {
-            LabelStatus.setText("! falsche Angaben !");
+                text1 += Usernameline;
+                Usernameline = UsernameReader.readLine();
+            }
+            while (Passwordline !=null){
+
+                text2 += Passwordline;
+                Passwordline = PasswordReader.readLine();
+
+            }
+
+
+            if (TextUserName.getText().equals(text1) && TextPassword.getText().equals(text2)) {
+
+                Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/MainWindow.fxml"));
+                Stage PrimeNet = new Stage();
+                PrimeNet.setTitle("PrimeNet");
+                Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+                //PrimeNet.setResizable(false);
+                //PrimeNet.setScene(new Scene(root, screen.width, screen.height));
+                PrimeNet.setScene(new Scene(root, screen.width / 1.5, screen.height / 1.1 - 65));
+                //PrimeNet.setScene(new Scene(root,800,600));
+                PrimeNet.show();
+                Main.Login.close();
+            } else {
+                LabelStatus.setText("! falsche Angaben !");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
 
-    //function of the reset button in login menu
-    public void Reset(javafx.event.ActionEvent event) {
-        TextUserName.setText("");
-        TextPassword.setText("");
-        LabelStatus.setText("Nochmal eingeben");
+
+        //function of the reset button in login menu
+        public void Reset (javafx.event.ActionEvent event){
+            TextUserName.setText("");
+            TextPassword.setText("");
+            LabelStatus.setText("Nochmal eingeben");
+        }
+
+
+        public void openCreateUser (javafx.event.ActionEvent event) throws Exception {
+
+            Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/CreateUserWindow.fxml"));
+            Stage CreateUser = new Stage();
+            CreateUser.setTitle("Create_User");
+            CreateUser.setScene(new Scene(root, 250, 150));
+            CreateUser.show();
+
+        }
+
+
+        @Override
+        public void initialize (URL location, ResourceBundle resources){
+
+
+        }
     }
 
-
-    public void openCreateUser(javafx.event.ActionEvent event) throws Exception{
-
-        Parent root = FXMLLoader.load(getClass().getResource("FxmlFiles/CreateUserWindow.fxml"));
-        Stage CreateUser = new Stage();
-        CreateUser.setTitle("Create_User");
-        CreateUser.setScene(new Scene(root,250,150));
-        CreateUser.show();
-
-    }
-
-
-
-    @Override
-    public void initialize(URL location, ResourceBundle resources) {
-
-
-    }
-}
 
 
 
